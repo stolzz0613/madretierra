6de807c544f94d00e0aa6c30f144c0c2fc317962
Andres <Andres0613@utp.edu.co>
2023-12-09 19:15:35 -0500

    fix order highlight

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,9 +1,11 @@
+import { useState } from 'react'
 import { useGlobalState } from '../context/GlobalContext'
 import Slide from './Highlights/Slide'
 
 function Results() {
   const { state } = useGlobalState()
   const { dispatch } = useGlobalState()
+  const [order, setorder] = useState('')
 
   return (
     <div className="border-solid border-2 border-zinc-200 rounded-lg shadow-xl min-h-24">
@@ -13,8 +15,27 @@ function Results() {
         </h1>
         <p className='text-left my-8'>Ordenar por:</p>
         <div className='flex flex-row gap-4'>
-          <p onClick={() => dispatch({ type: 'DESC_ORDER' })} className='cursor-pointer border-solid border-2 border-zinc-200 rounded-lg px-2'>↓ Menor Precio</p>
-          <p onClick={() => dispatch({ type: 'ASC_ORDER' })} className='cursor-pointer border-solid border-2 border-zinc-200 rounded-lg px-2'>↑ Mayor Precio</p>
+          <p
+            onClick={() => {
+              dispatch({ type: 'DESC_ORDER' })
+              setorder('menor')
+            }}
+            className='cursor-pointer border-solid border-2 border-zinc-200 rounded-lg px-2'
+            style={{
+              borderColor: order === 'menor' ? '#6AA843' : 'rgb(228, 228, 231)'
+            }}
+          >↓ Menor Precio
+          </p>
+          <p
+            onClick={() => {
+              dispatch({ type: 'ASC_ORDER' })
+              setorder('mayor')
+            }}
+            style={{
+              borderColor: order === 'mayor' ? '#6AA843' : 'rgb(228, 228, 231)'
+            }}
+            className='cursor-pointer border-solid border-2 border-zinc-200 rounded-lg px-2'
+          >↑ Mayor Precio</p>
         </div>
       </div>
       <div className="m-6 mt-12 grid grid-cols-4 gap-4">
